d3311319fc8913474e8f41485b8c890d14f81f52
TaylorChavis <101995876+TaylorChavis@users.noreply.github.com>
2025-11-21 21:29:06 -0500

    Revert "userResource"

--- src/server.js
+++ src/server.js
@@ -2,9 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 
-import userRoutes from './routes/userRoutes.js';
-import authRoutes from './src/routes/authRoutes.js';
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
@@ -12,9 +9,6 @@ app.use(cors());
 app.use(morgan('tiny'));
 
 app.use(express.json());
-
-app.use('/api/users', userRoutes);
-app.use('/auth', authRoutes);  
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
