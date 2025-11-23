6bace2bfb358185b21dd1ad11763a7fba466cf5b
Nathan Finney <nfinney@charlotte.edu>
2025-11-23 13:15:35 -0500

    Added a validators folder, a serviceValidator.js file, and made appropiate changes to the serviceRoutes.js file

---
+++ src/routes/serviceRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../middleware/auth");
+import { validateServiceData } from "../validators/serviceValidator.js";
+
+const {
+  getAllServices,
+  getServiceById,
+  createService,
+  updateService,
+  deleteService,
+} = require("../controllers/services.controller");
+
+// All routes here require authentication
+router.get("/", auth, getAllServices);
+router.get("/:id", auth, getServiceById);
+router.post("/", auth, createService);
+router.put("/:id", auth, updateService);
+router.delete("/:id", auth, deleteService);
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -3,7 +3,8 @@ import cors from 'cors';
 import morgan from 'morgan';
 
 import userRoutes from './routes/userRoutes.js';
-import authRoutes from './src/routes/authRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import serviceRoutes from './routes/serviceRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,7 +15,8 @@ app.use(morgan('tiny'));
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
-app.use('/auth', authRoutes);  
+app.use('/auth', authRoutes);
+app.use('/services', serviceRoutes);
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
